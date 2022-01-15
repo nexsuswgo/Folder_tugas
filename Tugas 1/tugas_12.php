f90a8b0b7347e3f3b01a28e03ac5fb3c9ae266f1
siti khotifah <90820052+nexsuswgo@users.noreply.github.com>
2022-01-15 21:20:52 +0700

    uas

---
+++ Tugas 1/tugas_12.php
@@ -0,0 +1,30 @@
+<?php
+
+echo " moci adalah kucing<br>";
+echo "punya ekor sebanyak : 1<br>";
+echo "tidak bisa terbang<br>";
+echo "suaranya:meong-meong";
+
+echo "<hr>";
+
+echo "peng adalah sapi<br>";
+echo "punya kaki sebanyak : 4<br>";
+echo "tidak bisa terbang<br>";
+echo "suaranya : maarr";
+
+echo "<hr>";
+
+echo "chicken adalah ayam<br>";
+echo "punya sayap sebanyak : 2<br>";
+echo "suka makan pur<br>";
+echo "suaranya: kukkruyuk";
+
+echo "<hr>";
+
+echo "boou adalah kerbau<br>";
+echo "punya ekor: 1<br>";
+echo "merupakan hewan gagah<br>";
+echo "suaranya : mooo";
+
+echo "<hr>";
+?>
